e45af0d65b6048a89c38509dc2e86b35289e7806
Michelle Hoelzer <hoelzermm@gmail.com>
2018-12-18 10:37:38 -0500

    Updates

---
+++ precache-manifest.a6e6c5f82fff83b3efc0e989d1f912a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a8545fe2960f22b50b",
+    "url": "/birdNerd/static/css/main.7edb5f0b.chunk.css"
+  },
+  {
+    "revision": "64a8545fe2960f22b50b",
+    "url": "/birdNerd/static/js/main.64a8545f.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/birdNerd/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "f1ae7ce26f38d0ec0d80",
+    "url": "/birdNerd/static/js/runtime~main.f1ae7ce2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/birdNerd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e5867d1408bf6e6eb4d89ab487958b46",
+    "url": "/birdNerd/index.html"
+  }
+];
