53a5ecf329b9594b88b80f873710c02dd8e101c4
Austin Levine <austinlevine4@gmail.com>
2019-01-12 00:32:43 -0500

    centered tag line with <center></center> -.- and added information (question mark button) to homepage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -13,7 +13,8 @@ import {
   Responsive,
   Segment,
   Sidebar,
-  Visibility
+  Visibility,
+  Modal
 } from "semantic-ui-react";
 // import { Homepage } from "../Actions/action.js";
 // import NavBarSecondary from "./NavBarSecondary";
@@ -28,31 +29,46 @@ class Homepage extends Component {
   componentWillUnmount = () => {
     document.body.classList.remove("background");
   };
-  
+
   render() {
     return (
       <div>
         <div className="headers">
           <h1>Welcome to Bird Nerd</h1>
-          <h2>Where nerds of a feather bird together</h2>
+          <center>
+            <h2>Where nerds of a feather bird together</h2>
+          </center>
         </div>
 
         <SearchBar className="search" />
-        <div className="para">
-          We had two goals in mind when creating Bird Nerd:
-          <ol>
-            <li>Give users an easy way to identify birds they have see</li>
-            <li>Allow users to document their sightings</li>
-          </ol>
-          On this site, users can both identify birds they have seen and
-          document their sightings in a personal notebook. Going to our
-          “Advanced Search” page allows the user to identify birds, with our
-          code filtering the results based on criteria such as color, size, and
-          location. Our “Birdydex” contains information on all U.S. birds, users
-          should browse at their own risk. Creating a profile allows a user
-          access to a notebook where they can store information on sightings,
-          such as the species and the time and date of the sighting.
-          <h2 className="sub-header">Happy birding!</h2>
+
+        <div className="about">
+          <Modal trigger={<Icon name="massive question circle outline" />}>
+            <Modal.Header>Information</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                <Header>Goals</Header>
+                <ol>
+                  <li>
+                    Give users an easy way to identify birds they have see
+                  </li>
+                  <li>Allow users to document their sightings</li>
+                </ol>
+                <Header>About</Header>
+                <p>
+                  On this site, users can both identify birds they have seen and
+                  document their sightings in a personal notebook. Going to our
+                  “Advanced Search” page allows the user to identify birds, with
+                  our code filtering the results based on criteria such as
+                  color, size, and location. Our “Birdydex” contains information
+                  on all U.S. birds, users should browse at their own risk.
+                  Creating a profile allows a user access to a notebook where
+                  they can store information on sightings, such as the species
+                  and the time and date of the sighting.
+                </p>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
         </div>
       </div>
     );
