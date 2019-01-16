0e9ca929671cb9a33f00c56b81b71fa244c21ab3
Michelle Hoelzer <hoelzermm@gmail.com>
2019-01-16 12:30:37 -0500

    navbar fixes; eddited logout to say birdnerd; some css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {
   Container,
+  Dropdown,
   Icon,
   Image,
   Menu,
@@ -106,88 +107,26 @@ export default class Navbar extends Component {
           </div>
         </Responsive>
         <Responsive maxWidth={767}>
-          <Menu stackable>
-            <Menu.Item
-              onClick={() =>
-                this.setState({ menuVisible: !this.state.menuVisible })
-              }
-            >
-              <Icon name="sidebar" />
-              Menu
-            </Menu.Item>
-            <Sidebar.Pushable as={Segment} attached="bottom">
-              <Sidebar
-                as={Menu}
-                animation="uncover"
-                visible={this.state.menuVisible}
-                icon="labeled"
-                vertical
-                inline
-                inverted
-              >
-                <Menu.Item
-                  exact
-                  as={NavLink}
-                  to="/"
-                  name="home"
-                  // active={activeItem === "home"}
-                  // onClick={this.handleItemClick}
-                >
-                  <img src={logo} alt="Logo" />
-                </Menu.Item>
-                {/* <NavbarItems to={this.state.navbarItems.pathname}>
-            {this.state.navbarItems.name}
-          </NavbarItems> */}
-                <Menu.Item
-                  exact
-                  as={NavLink}
-                  to="/birdidex"
-                  name="birdidex"
-                  // active={activeItem === "birdidex"}
-                  // onClick={this.handleItemClick}
-                >
-                  Birdidex
-                </Menu.Item>
-                <Menu.Item
-                  exact
-                  as={NavLink}
-                  to="/advanced"
-                  name="advanced"
-                  // active={activeItem === "advanced"}
-                  // onClick={this.handleItemClick}
-                >
-                  Advanced Search
-                </Menu.Item>
-
-                {/* <Menu.Item position={"right"}>
-            <NavLink to="/username">Username</NavLink>
-          </Menu.Item>
- */}
-                <Menu.Menu position="right">
-                  <Menu.Item
-                    exact
-                    as={NavLink}
-                    to="/login"
-                    name="login"
-                    // active={activeItem === "login"}
-                    // onClick={this.handleItemClick}
-                  >
-                    Login
-                  </Menu.Item>
-                  <Menu.Item
-                    exact
-                    as={NavLink}
-                    to="/register"
-                    name="register"
-                    // active={activeItem === "register"}
-                    // onClick={this.handleItemClick}
-                  >
-                    Register
-                  </Menu.Item>
-                </Menu.Menu>
-              </Sidebar>
-            </Sidebar.Pushable>
-          </Menu>
+          <Dropdown icon="sidebar" style={{fontSize: "50px", marginTop: "20px"}}>
+            <Dropdown.Menu >
+              <Dropdown.Item exact as={NavLink} to="/" name="home" className="dropdownItem">
+                <img src={logo} alt="Logo" /> Home
+              </Dropdown.Item>
+              <Dropdown.Item exact as={NavLink} to="/birdidex" name="birdidex" className="dropdownItem">
+                Birdidex
+              </Dropdown.Item>
+              <Dropdown.Item exact as={NavLink} to="/advanced" name="advanced" className="dropdownItem">
+                Advanced Search
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item exact as={NavLink} to="/login" name="login" className="dropdownItem">
+                Login
+              </Dropdown.Item>
+              <Dropdown.Item exact as={NavLink} to="/register" name="register" className="dropdownItem">
+                Register
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Responsive>
       </React.Fragment>
     );
