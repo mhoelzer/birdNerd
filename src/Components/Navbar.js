e6b55b57d32341edefc9da8f650dd0138f942014
Michelle Hoelzer <hoelzermm@gmail.com>
2019-01-16 15:37:30 -0500

    finished basic mobile navbar; need to do switch of login/out for mbile and regualr

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,27 +15,9 @@ import { NavLink } from "react-router-dom";
 import "../Styling/main.css";
 
 export default class Navbar extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     navbarItems: [
-  //       { name: "Compose Entry", pathname: "/composeEntry" },
-  //       { name: "Login", pathname: "/login" },
-  //       { name: "Register", pathname: "/register" },
-  //       { name: "Search", pathname: "/" },
-  //       { name: "Advanced Search", pathname: "/advancedSearch" },
-  //       { name: "Birdidex", pathname: "/birdidex" },
-  //       { name: "Profile", pathname: "/profile" }
-  //     ]
-  //   };
-  // }
   state = { menuVisible: false };
-  // state = { activeItem: "home" };
-
-  // handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    // const { activeItem } = this.state;
     return (
       <React.Fragment>
         <Responsive minWidth={768}>
@@ -47,21 +29,14 @@ export default class Navbar extends Component {
                   as={NavLink}
                   to="/"
                   name="home"
-                  // active={activeItem === "home"}
-                  // onClick={this.handleItemClick}
                 >
                   <img src={logo} alt="Logo" />
                 </Menu.Item>
-                {/* <NavbarItems to={this.state.navbarItems.pathname}>
-            {this.state.navbarItems.name}
-          </NavbarItems> */}
                 <Menu.Item
                   exact
                   as={NavLink}
                   to="/birdidex"
                   name="birdidex"
-                  // active={activeItem === "birdidex"}
-                  // onClick={this.handleItemClick}
                 >
                   Birdidex
                 </Menu.Item>
@@ -70,24 +45,15 @@ export default class Navbar extends Component {
                   as={NavLink}
                   to="/advanced"
                   name="advanced"
-                  // active={activeItem === "advanced"}
-                  // onClick={this.handleItemClick}
                 >
                   Advanced Search
                 </Menu.Item>
-
-                {/* <Menu.Item position={"right"}>
-            <NavLink to="/username">Username</NavLink>
-          </Menu.Item>
- */}
                 <Menu.Menu position="right">
                   <Menu.Item
                     exact
                     as={NavLink}
                     to="/login"
                     name="login"
-                    // active={activeItem === "login"}
-                    // onClick={this.handleItemClick}
                   >
                     Login
                   </Menu.Item>
@@ -96,8 +62,6 @@ export default class Navbar extends Component {
                     as={NavLink}
                     to="/register"
                     name="register"
-                    // active={activeItem === "register"}
-                    // onClick={this.handleItemClick}
                   >
                     Register
                   </Menu.Item>
@@ -107,22 +71,56 @@ export default class Navbar extends Component {
           </div>
         </Responsive>
         <Responsive maxWidth={767}>
-          <Dropdown icon="sidebar" style={{fontSize: "50px", marginTop: "20px", marginLeft: "30px"}} >
-            <Dropdown.Menu style={{minWidth: "300px"}}  >
-              <Dropdown.Item exact as={NavLink} to="/" name="home" >
+          <Dropdown
+            icon="sidebar"
+            style={{ fontSize: "50px", marginTop: "30px", marginLeft: "10px" }}
+          >
+            <Dropdown.Menu style={{ minWidth: "200px" }}>
+              <br />
+              <Dropdown.Item
+                exact
+                as={NavLink}
+                to="/"
+                name="home"
+                style={{ fontSize: "20px", marginBottom: "10px" }}
+              >
                 <img src={logo} alt="Logo" /> Home
               </Dropdown.Item>
-              <Dropdown.Item exact as={NavLink} to="/birdidex" name="birdidex" className="dropdownItem">
+              <Dropdown.Item
+                exact
+                as={NavLink}
+                to="/birdidex"
+                name="birdidex"
+                style={{ fontSize: "20px", marginBottom: "10px" }}
+              >
                 Birdidex
               </Dropdown.Item>
-              <Dropdown.Item exact as={NavLink} to="/advanced" name="advanced" className="dropdownItem">
+              <Dropdown.Item
+                exact
+                as={NavLink}
+                to="/advanced"
+                name="advanced"
+                style={{ fontSize: "20px" }}
+              >
                 Advanced Search
               </Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item exact as={NavLink} to="/login" name="login" className="dropdownItem">
+              <Dropdown.Item
+                exact
+                as={NavLink}
+                to="/login"
+                name="login"
+                style={{ fontSize: "20px", marginBottom: "10px" }}
+              >
                 Login
               </Dropdown.Item>
-              <Dropdown.Item exact as={NavLink} to="/register" name="register" className="dropdownItem">
+              <Dropdown.Item
+                exact
+                as={NavLink}
+                to="/register"
+                name="register"
+                style={{ fontSize: "20px" }}
+              >
                 Register
               </Dropdown.Item>
             </Dropdown.Menu>
